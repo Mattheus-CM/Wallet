1c5884ebc0acd76e84462d6fd5e9346828267132
Mattheus Martins <mattheus_cmartins@hotmail.com>
2023-09-11 15:19:49 -0300

    Criando o diretório e arquivo de rotas

--- App.js
+++ App.js
@@ -1,23 +1,7 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Login from "./src/screens/Login";
-import Home from "./src/screens/Home";
-import Register from "./src/screens/Register";
-import Profile from "./src/screens/Profile";
-import Transferencia from "./src/screens/Transferencia";
-
-const Stack = createNativeStackNavigator();
+import Routes from "./src/routes/Routes";
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Register" component={Register} />
-        <Stack.Screen name="Home" component={Home}/>
-        <Stack.Screen name="Profile" component={Profile} />
-        <Stack.Screen name="Transferencia" component={Transferencia} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Routes />
   );
 }
--- src/screens/Profile.jsx
+++ src/screens/Profile.jsx
@@ -1,11 +1,23 @@
-import { View } from "react-native";
+import { Text, View } from "react-native";
+import Perfil from "../components/Perfil";
+
+const Person = {
+  nome: "Mattheus",
+  imagem:
+    "https://img.quizur.com/f/img62dc75c68ada20.76829981.jpeg?lastEdited=1658615257",
+  saldo: 1234,
+  entradas: 19.0,
+  saidas: 4.5,
+  balanco: [5, -4.5, 14],
+};
 
 function Profile() {
-    return(
-        <View>
-            <Text>Tela Perfil</Text>
-        </View>
-    );
+  return (
+    <View>
+      <Text>Tela Perfil</Text>
+      <Perfil nome={Person.nome} imagem={Person.imagem} />
+    </View>
+  );
 }
 
 export default Profile;
